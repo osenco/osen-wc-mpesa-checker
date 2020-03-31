fcc91cf84a14dc4d985919e6467b67b1db35b75c
Mauko Maunde <maukoese@gmail.com>
2020-03-31 12:36:57 +0300

    Update osen-wc-mpesa-checker.php

--- osen-wc-mpesa-checker.php
+++ osen-wc-mpesa-checker.php
@@ -1,15 +1,15 @@
 <?php
 
 /**
- * @package Hello_Dolly
- * @version 1.7.2
+ * @package Mpesa Checker
+ * @version 1.20.4
  */
 /*
 Plugin Name: Mpesa Transaction Checker
 Plugin URI: http://wordpress.org/plugins/hello-dolly/
 Description: This is not just a plugin, it symbolizes the hope and enthusiasm of an entire generation summed up in two words sung most famously by Louis Armstrong: Hello, Dolly. When activated you will randomly see a lyric from <cite>Hello, Dolly</cite> in the upper right of your admin screen on every page.
 Author: Mauko Maunde
-Version: 1.7.2
+Version: 1.20.4
 Author URI: http://ma.tt/
 */
 
